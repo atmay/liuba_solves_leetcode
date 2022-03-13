704f83323a5454004173850449847c9ed4352e40
atmay <atmay@yandex.ru>
2022-03-13 21:30:30 +0300

    problem explanation added

--- two_pointers/167_two_sum_ii.py
+++ two_pointers/167_two_sum_ii.py
@@ -1,14 +1,33 @@
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+find two numbers such that they add up to a specific target number. Let these two numbers
+be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+"""
+
+
 def two_sum(numbers: list[int], target: int) -> list[int]:
     """Two pointers solution: go from both ends till pointers met"""
-    l, r = 0, 1
-    while (numbers[l] + numbers[r]) != target:
-        if numbers[l] + numbers[r] < target:
-            if r < len(numbers) - 1:
-                r += 1
-            else:
-                l += 1
-                r = l + 1
-    return [l + 1, r + 1]
+    left, right = 0, len(numbers) - 1
+    while left < right:
+        t = numbers[left] + numbers[right]
+        if t == target:
+            return [left + 1, right + 1]
+        elif t < target:
+            left += 1
+        else:
+            right -= 1
 
 
 if __name__ == '__main__':

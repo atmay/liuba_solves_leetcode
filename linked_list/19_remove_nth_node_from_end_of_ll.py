7cf8bf552f2137e6dc5008ade71d2371c626ccd4
atmay <atmay@yandex.ru>
2022-03-15 23:51:36 +0300

    linked list problem solved

---
+++ linked_list/19_remove_nth_node_from_end_of_ll.py
@@ -0,0 +1,30 @@
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def remove_nth_from_end(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    """I used two pointers approach. When rightmost pointer hits end,
+    reassign links for the next node for the leftmost pointer"""
+    l = r = head
+    for _ in range(n):
+        r = r.next
+    if not r:
+        return head.next
+    while r.next:
+        r = r.next
+        l = l.next
+    l.next = l.next.next
+    return head
